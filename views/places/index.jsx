29ae8c431c574e61e48c5a737aa7eefb6b1b7fc8
zig <zvszig@gmail.com>
2023-11-15 19:17:55 -0800

    added style to form

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -7,7 +7,7 @@ function index(data) {
       <div className="col-sm-6">
         <h2>{place.name}</h2>
         <p className="text-center">{place.cuisines}</p>
-        <img src={place.pic} alt={place.name} width="250px" height="250px"/>
+        <img src={place.pic} alt={place.name} width="250px" height="250px" class="center"/>
         <p className="text-center">
           Located in {place.city}, {place.state}
         </p>
