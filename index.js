36e0c6a1c8efb1636ef41ace1b07fdc822d355d0
zig <zvszig@gmail.com>
2023-11-18 11:02:37 -0800

    added delete implementation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
+
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 app.use('/places', require('./controllers/places'))
+
 
 
 
