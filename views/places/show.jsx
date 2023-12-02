9c1897a7c0effb2e346226bddea2aaafada2e15b
zig <zvszig@gmail.com>
2023-12-01 23:27:19 -0800

    finished part 10

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,21 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! ðŸ˜¡" : "Rave! ðŸ˜»"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (
     <Def>
       <main>
@@ -33,8 +48,54 @@ function show(data) {
         <br />
         <div className="row">
           <h2>Comments</h2>
-          <p>no comments yet!</p>
+          {comments}
         </div>
+
+        
+        {/*This is the form for the comments  */}
+        <form action={`/places/${props.place.id}/comment`} method="POST">
+          <div className="form-group">
+            <label htmlFor="author">Author</label>
+            <input
+              type="text"
+              id="author"
+              name="author"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content">Content</label>
+            <textarea
+              id="content"
+              name="content"
+              className="form-control"
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <label htmlFor="starRating">Star Rating</label>
+            <input
+              type="number"
+              id="starRating"
+              name="starRating"
+              step="0.5"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              id="rant"
+              name="rant"
+              className="form-check-input"
+            />
+            <label className="form-check-label" htmlFor="rant">
+              Rant
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
         <br />
         <br />
         <div className="row">
